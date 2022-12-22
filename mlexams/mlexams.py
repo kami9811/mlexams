8397661d2b07d65a9dd4767645060133b14fc18c
yuta.kawakami <kami.981108@gmail.com>
2022-12-23 06:11:55 +0900

    Fixed for gpu settings

--- mlexams/mlexams.py
+++ mlexams/mlexams.py
@@ -227,6 +227,35 @@ def get_accuracy(
         #         p
         #     )
     
+    elif model_kind == "curf":
+        
+        # learning
+        if not on_gpu:
+            clf = RandomForestClassifier(**options)
+
+            clf.fit(train_data, train_label)
+
+            p = clf.predict(test_data)
+            accuracy = accuracy_score(
+                test_label,
+                p
+            )
+        else:
+            train_data = cp.asarray(train_data)
+            train_label = cp.asarray(train_label)
+            test_data = cp.asarray(test_data)
+            test_label = cp.asarray(test_label)
+
+            clf = cuRFC(**options)
+            
+            clf.fit(train_data, train_label)
+
+            p = clf.predict(test_data)
+            accuracy = cu_accuracy_score(
+                test_label,
+                p
+            )
+    
     elif model_kind == "mlp":
         
         # Dataset
